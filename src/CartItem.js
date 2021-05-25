8de24bb58c7a84b857e0e8662c6c5b373d5e269d
Sai-Tejaswi <chisate1998@gmail.com>
2021-05-25 12:13:15 +0530

    modify state

--- src/CartItem.js
+++ src/CartItem.js
@@ -12,7 +12,12 @@ class CartItem extends React.Component {
         
     }
     increaseQuantity = () => {
-        console.log('this',this.state);
+   // console.log('this',this.state);
+        this.setState((prevState)=> {
+            return {
+                qty : prevState.qty + 1
+            }
+        })
     }
 
     
